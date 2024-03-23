1efc6491cae73f172b313435452afbe658e9358d
Morgan Caron <caron.morgan91@gmail.com>
2024-03-23 17:31:13 +0100

    kind: moduleonly

--- packages/c/cpputils/xmake.lua
+++ packages/c/cpputils/xmake.lua
@@ -1,5 +1,5 @@
 package("cpputils")
-	set_kind("library")
+	set_kind("moduleonly")
 	set_homepage("https://github.com/MorganCaron/CppUtils")
 	set_description("C++ Utilities Library for compilers and various projects")
 	set_license("LGPL3")
